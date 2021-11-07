9117b41fb0e5c5d15cee8061a01e0313df0e78ce
Jacob Long <jclong98@gmail.com>
2021-11-06 20:05:36 -0700

    new home page and post styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,8 +10,41 @@ useHead({
 
     <div>
       <router-link
+        to="/posts"
+        class="
+          mt-10
+          p-4
+          rounded-lg
+          shadow-lg
+          font-semibold
+          hover:bg-blue-400
+          dark:(text-white
+          bg-blue-400
+          hover:bg-blue-500)
+          text-blue-800
+          bg-blue-300
+          focus:ring-3
+          ring-indigo-500
+          flex
+          items-center
+          justify-center
+        "
+      >
+        See json placeholder posts
+        <mdi-chevron-right class="ml-1" />
+      </router-link>
+      <router-link
         to="/about"
-        class="mt-10 p-4 rounded-lg shadow-lg font-semibold dark:text-white dark:bg-blue-400 text-blue-800 bg-blue-300 focus:ring-3 ring-indigo-500 flex items-center justify-center hover:bg-blue-400 dark:hover:bg-blue-500"
+        class="
+          flex
+          justify-center
+          items-center
+          mt-5
+          p-4
+          rounded-lg
+          font-semibold
+          hover:underline
+        "
       >
         Learn More About This Project
         <mdi-chevron-right class="ml-1" />
@@ -20,5 +53,4 @@ useHead({
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
--- src/pages/posts/index.vue
+++ src/pages/posts/index.vue
@@ -17,14 +17,35 @@ onMounted(getPosts)
       v-for="post in posts"
       :key="post.id"
       :to="`/posts/${post.id}`"
-      class="inline-block border border-gray-500 shadow-lg dark:bg-gray-900 rounded my-3 p-4"
+      class="
+        inline-block
+        border border-purple-500
+        shadow-lg
+        dark:(bg-gray-900
+        border-gray-700)
+        rounded
+        my-3
+        p-4
+        bg-white
+      "
     >
       <header class="flex justify-between gap-4 mb-5">
         <h2 class="text-size-xl">{{ post.title }}</h2>
         <div>
           <p
-            class="opacity-75 dark:text-purple-100 flex items-center justify-center rounded-full w-8 h-8"
-          >#{{ post.id }}</p>
+            class="
+              opacity-75
+              dark:text-purple-100
+              flex
+              items-center
+              justify-center
+              rounded-full
+              w-8
+              h-8
+            "
+          >
+            #{{ post.id }}
+          </p>
         </div>
       </header>
       <p>{{ post.body }}</p>
